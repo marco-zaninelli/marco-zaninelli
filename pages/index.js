0a7d3773f586fb06dba9b86ac4160bfa0ed9e388
Marco Zaninelli <marco@zaninelli.me>
2024-12-20 22:18:53 +1100

    Construction page created

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home () {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main className={"w-full h-screen flex justify-center items-center flex-col"}>
-                <h1 className={"text-4xl font-mono"}>Website under construction</h1>
+                <h1 className={"text-4xl font-mono p-4"}>Website under construction</h1>
                 <p className={"font-mono p-4"}>Thanks for your patience while I debug my life... and a few websites along the way.</p>
             </main>
         </>
