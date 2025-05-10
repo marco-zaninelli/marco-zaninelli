4eb708c91d6c6a0582504669ca109ca9d9f11e8e
Marco Zaninelli <marco@zaninelli.me>
2025-05-10 20:02:26 +1000

    Removed middleware and implemented native nextjs's i18n

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,12 @@
 import Head from "next/head";
-import HomeLink from "@/components/general/HomeLink";
+import {useRouter} from "next/router";
 import HomeInspect from "@/components/general/HomeInspect";
+import HomeLink from "@/components/general/HomeLink";
 
-export default function HomeComponent ({isEnglish}) {
+export default function Home () {
+    const {locale} = useRouter();
+    const isEnglish = locale === "en";
+
     return (
         <>
             <Head>
@@ -11,24 +15,24 @@ export default function HomeComponent ({isEnglish}) {
             </Head>
             <div className={"w-full h-screen flex justify-center items-center flex-col px-10 sm:px-20"}>
                 <h2 className={"w-full text-center max-w-screen-xl leading-relaxed"}>
-                {isEnglish ? (
-                    <>
+                    {isEnglish ? (
+                        <>
                             Hi, I'm Marco, a <HomeInspect
                             tooltip={"I mix code and design to create interactive websites, apps, and digital art. I turn creative ideas into functional, engaging experiences using both tech and visual skills."}>Creative
                                                                                                                                                                                                                     Developer</HomeInspect> passionate
                             about building great experiences.<br />
                             The <HomeLink href={"#"}>Portfolio</HomeLink> shows my craft and the <HomeLink
                             href={"#"}>Gallery</HomeLink> shows my soul. Let’s start a <HomeLink href={"#"}>Collaboration</HomeLink>.
-                    </>
-                ) : (
-                    <>
+                        </>
+                    ) : (
+                        <>
                             Ciao, sono Marco, uno <HomeInspect
                             tooltip={"Scrivo codice e realizzo design per creare siti web interattivi, app e arte digitale. Trasformo idee creative in esperienze funzionali e coinvolgenti, sfruttando competenze tecniche e visive."}>Sviluppatore Creativo</HomeInspect> e creo esperienze digitali innovative.<br />
                             Il <HomeLink href={"#"}>Portfolio</HomeLink> mostra le mie realizzazioni e la <HomeLink href={"#"}>Galleria</HomeLink> mostra la
                             mia creatività. Iniziamo una <HomeLink href={"#"}>Collaborazione</HomeLink> assieme.
-                    </>
-                )
-                }
+                        </>
+                    )
+                    }
                 </h2>
             </div>
         </>
