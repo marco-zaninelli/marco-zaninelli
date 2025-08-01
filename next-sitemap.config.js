9857ab11d1b45bb1b2bfc0f016d95b9d44150997
Marco Zaninelli <marco@zaninelli.me>
2025-08-01 20:52:11 +0800

    Added dynamic sytemap, and page headers to all pages

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,15 +1,32 @@
-
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://www.marco.zaninelli.me',
     generateRobotsTxt: true,
     exclude: ['/studio/*', '/api/*'],
     robotsTxtOptions: {
+        additionalSitemaps: [
+            'https://www.marco.zaninelli.me/api/server-sitemap.xml'
+        ],
         policies: [
             {
                 userAgent: '*',
                 disallow: ['/studio/', '/api/'],
+                allow: '/',
             },
         ],
     },
+    // Add these important configurations
+    changefreq: 'daily',
+    priority: 0.7,
+    sitemapSize: 7000,
+    alternateRefs: [
+        {
+            href: 'https://www.marco.zaninelli.me',
+            hreflang: 'it',
+        },
+        {
+            href: 'https://www.marco.zaninelli.me/en',
+            hreflang: 'en',
+        },
+    ],
 }
